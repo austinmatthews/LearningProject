24a82132e2ef3f6def22deee3b77753d02b5da63
austinmatthews <ausmatt14@gmail.com>
2017-09-12 22:27:57 -0400

    Added network requests in BlueFletchClient

--- app/src/main/java/io/matthews/austin/bluefletchlearningproject/BlueFletchClient.java
+++ app/src/main/java/io/matthews/austin/bluefletchlearningproject/BlueFletchClient.java
@@ -6,9 +6,68 @@ import retrofit2.http.*;
 
 public interface BlueFletchClient {
 @FormUrlEncoded
+
+    //POST REQUESTS
+
+    //Login User
     @POST("/login")
+    //Call object lets us do this asynchronously
     Call<LoginResult> login(
         @Field("username") String uname,
         @Field("password") String password
     );
+/*
+    //Create a new comment
+    @POST("/comment")
+    Call<LoginResult> login(
+        @Field("username") String uname,
+        @Field("postId") int postId,
+        @Field("commentText") String commentText
+    );
+*/
+    //Logout user and clear the session
+    @POST("/logout")
+    Call<ResponseBody> logout();
+/*
+    //Create a new post (thread)
+    @POST("/post")
+    Call<PostResult> post(
+        @Field("username") String uname,
+        @Field("postText") String postText
+    );
+
+    //GET REQUESTS
+
+    //Get posts and their comments
+    @GET("/feed")
+    Call<Test> testServer();
+
+    //Test the BlueFletch API
+    @GET("/test")
+    Call<Test> testServer();
+
+    //Get the information for the logged in user
+    @GET("/user")
+    Call<Test> testServer();
+
+    //Get the information for a specific user
+    @GET("/user/username/{username}")
+    Call<Test> testServer();
+
+
+    //PUT REQUESTS
+
+    //Update comment text
+    @PUT("/comment/{id}")
+    Call<Test> testServer();
+
+    //Update post text
+    @PUT("/post/{id}")
+    Call<Test> testServer();
+
+    //Upload a new profile picture
+    @PUT("/user/{username}/profilepic")
+    Call<Test> testServer();
+
+*/
 }
--- app/src/main/java/io/matthews/austin/bluefletchlearningproject/MainActivity.java
+++ app/src/main/java/io/matthews/austin/bluefletchlearningproject/MainActivity.java
@@ -4,7 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Context;
 
+
+import com.franmontiel.persistentcookiejar.ClearableCookieJar;
+import com.franmontiel.persistentcookiejar.PersistentCookieJar;
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
+
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -22,9 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView)findViewById(R.id.blueFletchTextView);
 
+        ClearableCookieJar cookieJar = new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(this));
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .cookieJar(cookieJar)
+                .build();
+
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("https://bfapp-bfsharing.rhcloud.com/")
-                .addConverterFactory(GsonConverterFactory.create());
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient);
 
         Retrofit retrofit = builder.build();
 
@@ -48,5 +63,29 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "ERROR :(", Toast.LENGTH_SHORT).show();
             }
         });
+
+        /*
+
+        System.out.println("STRING ONE");
+        Call<ResponseBody> call1 = client.logout();
+        System.out.println("STRING TWO");
+
+        call1.enqueue(new Callback<ResponseBody>() {
+
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.code() == 200){
+                    textView.setText("LOGOUT WORKED!");
+                }
+                else{
+                    System.out.println("LOGOUT FAILED I GUESS");
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "ERROR :(", Toast.LENGTH_SHORT).show();
+            }
+        });
+        */
     }
 }
